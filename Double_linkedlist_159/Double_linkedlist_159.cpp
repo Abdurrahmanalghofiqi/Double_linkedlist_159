e32e7c2ec11147be89a137a6091aac124ba94934
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-05-26 17:00:45 +0700

    Add project files.

---
+++ Double_linkedlist_159/Double_linkedlist_159.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+	int noMhs;
+	string name;
+	Node* next;
+	Node* prev;
+};
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int eollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traversa();
+	void revtraverse();
+	void hapus();
+	void searchData();
+};
+
+DoubleLinkedList::DoubleLinkedList() {
+	START = NULL;
+}
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student; ";
+	cin >> nim;
+	cout << "\nEnter the name of the student; ";
+	cin >> nm;
+	Node* newNode = new Node();	//step 1
+	newNode->noMhs = nim;	//step 2
+	newNode->name = nm;	//step 2
+
+	/*insert a node in the beginning of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDrplicata number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;	//step3
+		if (START != NULL)
+			START->prev = newNode;	//step4
+		newNode->prev = NULL;	//step5
+		START = newNode;	//step6
+		return;
+	}
+
+	/*inserting a Node Between Two Nodes in the List*/
+	Node* current = START;	//step 1.a
+	Node* previous = NULL;	//step 1.b
+	while (current->next != NULL && current->next->noMhs < nim)	//step 1.c
+	{
+		previous = current;	//1.d
+		current = current->next;	//1.e
+	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
+	newNode->next = current->next;	//step 4
+	newNode->prev = current;	//step5
+	if (current->next != NULL)
+		current->next->prev = newNode;	//step 6
+	current->next = newNode;	//step 7
+}
+
+bool DoubleLinkedList::search(int ro)
